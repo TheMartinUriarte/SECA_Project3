bfdefc3947e0cd61092529a67e19678304bcf632
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-02 14:42:22 -0700

    [REFACTOR] Refactored DemoApplication.java and DemoApplicationTests.java to UsersApiApplication.java, UserApiApplicationTests.java

---
+++ nyc-open-data/src/main/java/com/example/nycopendata/controllers/UsersController.java
@@ -0,0 +1,10 @@
+package com.example.nycopendata.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+
+    @GetMapping("/users")
+}
