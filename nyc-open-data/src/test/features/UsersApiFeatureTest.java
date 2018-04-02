60a88a141e4b6c26b4c48c55d1c4d7036fdbdb25
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-02 16:37:28 -0700

    [CONFIG] Added junit annotations for some action steps

--- nyc-open-data/src/test/features/UsersApiFeatureTest.java
+++ nyc-open-data/src/test/features/UsersApiFeatureTest.java
@@ -2,4 +2,17 @@
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class UsersApiFeatureTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @After
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
 }
