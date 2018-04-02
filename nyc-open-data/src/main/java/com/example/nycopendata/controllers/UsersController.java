d2efe11a7c170f6b39e4e94bf5549df69d76f93d
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-02 15:05:07 -0700

    [TEST] to see if configuration properly executed.

--- nyc-open-data/src/main/java/com/example/nycopendata/controllers/UsersController.java
+++ nyc-open-data/src/main/java/com/example/nycopendata/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.example.nycopendata.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsersController {
 
     @GetMapping("/users")
+    public String home() {
+        return "some users";
+    }
 }
