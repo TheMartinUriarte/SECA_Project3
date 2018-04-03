e956f45a6e0969aab65dccda1d502c6b0d3e6bc4
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-02 17:37:38 -0700

    [WIP] Attempting to setup Flyway migrations so to see if it'll resolve the postgres issue when docker-compose up shuts down

--- nyc-open-data/src/test/features/UsersApiFeatureTest.java
+++ nyc-open-data/src/test/features/UsersApiFeatureTest.java
@@ -15,4 +15,33 @@ public class UsersApiFeatureTest {
         userRepository.deleteAll();
     }
 
+    @Test
+    public void shouldAllowFullCrudForAUser() throws Exception {
+
+        User firstUser = new User(
+                "Ash",
+                "Ketchum",
+                "Trainer"
+        );
+
+        User secondUser = new User(
+                "Misty",
+                "Yawa",
+                "Gym Leader"
+        );
+
+        Stream.of(firstUser, secondUser)
+                .forEach(user -> {
+                    userRepository.save(user);
+                });
+    }
+
 }
+
+// Test get all Users
+when()
+    .get("http://localhost:8080/users")
+.then()
+    .statusCode(is(200))
+    .and().body(containsString("Misty"))
+    .and().body(containsString("Yawa"));
