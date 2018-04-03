75bb953b582f00ed78ee693cf34c6fe04374116b
TheMartinUriarte <themartinuriarte.programmer@gmail.com>
2018-04-02 22:07:41 -0700

    [BUG] Module clan-up by removing duplicates

--- nyc-open-data/src/main/java/com/example/nycopendata/controllers/UsersController.java
+++ nyc-open-data/src/main/java/com/example/nycopendata/controllers/UsersController.java
@@ -1,7 +1,7 @@
 package com.example.nycopendata.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
